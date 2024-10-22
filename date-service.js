9fc50754bc61f4480d8cb1ea62d8887ac058c627
Ildar <ildar.1795@gmail.com>
2024-10-22 09:11:34 +0400

    init

---
+++ date-service.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send(new Date())
+})
+
+app.listen(3005, () => {
+	console.log('Ready')
+})
---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import fetch from 'node-fetch'
+const app = express()
+
+const DATE_SERVICE_HOST =
+	process.env.DATE_SERVICE_HOST || 'http://localhost:3005/'
+app.get('/', (req, res) => {
+	fetch(DATE_SERVICE_HOST)
+		.then(res => res.json())
+		.then(data => res.send(`Hello, current date is ${data}\n`))
+})
+
+app.listen(3000, () => {
+	console.log('Ready')
+})
